2b070d5d6ea350b3b76f3b7eb03281c2cb91ea34
af2111 <36798118+af2111@users.noreply.github.com>
2020-08-22 16:59:56 +0200

    fixed auto buy and removed autoclicker
    autoclicker doesn't work

--- CookieMngr.js
+++ CookieMngr.js
@@ -1,40 +1,14 @@
-function _dodoo(obj){
-	if($("html").data("click-click")){
-		clearInterval($("html").data("click-click"));
-		$("html").data("click-click","");
-		$("#click-click").css({
-			"background-color":"#eee",
-			"color":"black"
-		}).text("Start clicking");
-	}else{
-		$("html").data("click-click",setInterval(function(){
-			$("#bigCookie").trigger({type:"click",which:1});
-			if ($("#shimmers .shimmer").length>0 &&
-				!!(($("#shimmers .shimmer").css("background-image")||"").search("goldCookie.png")+1)
-			){
-				setTimeout(function(){$("#shimmers .shimmer").each(function(){
-					$(this).trigger({type:"click",which:1})},200);
-				});
-			}
-		}, 1));
-		$("#click-click").css({
-			"background-color":"red",
-			"color":"green"
-		}).text("Stop clicking");
-	}
-}
 (function(){
 	var script=document.createElement("SCRIPT");
 	script.src='https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js';
 	script.type='text/javascript';
 	script.onload=function(){
 		var $=window.jQuery;
-		$("#topBar").css("z-index","1000000").append("<button onclick='_dodoo()' style='position:absolute;padding:5px;margin:0;top:0;right:100px;height:32px;cursor:pointer;' id='click-click'>Start clicking</button>");
 		$("#topBar").append("<span style='position:absolute;padding:4px;margin:0;top:0;right:188px;height:20px;background-color:#eee;color:#000;border:2px #DDDDDD outset;'><input type='checkbox' id='dodoobuy' /><label for='dodoobuy'>auto buy?</label> (<label for='dodootop'>top</label><input type='checkbox' id='dodootop' />)</span>");
 		$("#topBar").append("<span style='position:absolute;padding:4px;margin:0;top:0;right:329px;height:20px;background-color:#eee;color:#000;border:2px #DDDDDD outset;'><input type='checkbox' id='dodooupgoo' /><label for='dodooupgoo'>auto upgrade?</label></span>");
-	$("#topBar").append("<button style='position:absolute;padding:5px;margin:0;top:0;right:444px;height:32px;background-color:#eee;color:#000;' onclick='if(confirm(\"This will grant you a shadow cheating achievment.\nContinue?\"))Game.OpenSesame();'>OPEN SESAME</button>");
+	$("#topBar").append("<button style='position:absolute;padding:5px;margin:0;top:0;right:444px;height:32px;background-color:#eee;color:#000;' onclick='if(confirm(\"This will grant you a shadow cheating achievment. Continue?\"))Game.OpenSesame();'>OPEN SESAME</button>");
 		setInterval(function(){
-		chk=$("<input />").css({
+		var chk=$("<input />").css({
 				"position":"absolute",
 				"width":"30px",
 				"height":"30px",
@@ -44,7 +18,7 @@ function _dodoo(obj){
 				"margin":"0",
 				"z-index":1000000
 			}).attr("class","buyque").prop({"type":"checkbox","checked":false});
-			checkall=$("<input />").css({
+			var checkall=$("<input />").css({
 				"position":"absolute",
 				"width":"20px",
 				"height":"20px",
@@ -54,7 +28,7 @@ function _dodoo(obj){
 				"margin":"0",
 				"z-index":1000000
 			}).attr("class","checkall").prop({"type":"checkbox","checked":false});
-			inp=$("<input />").css({
+			var inp=$("<input />").css({
 				"position":"absolute",
 				"width":"30px",
 				"height":"20px",
@@ -64,7 +38,7 @@ function _dodoo(obj){
 				"margin":"0",
 				"z-index":1000000
 			}).attr({"class":"buyamnt","size":"3","value":"100"}).prop("type","text");
-			fldwn=$("<input />").css({
+			var fldwn=$("<input />").css({
 				"position":"absolute",
 				"width":"20px",
 				"height":"20px",
@@ -86,12 +60,12 @@ function _dodoo(obj){
 						event.stopPropagation();
 					}).on("keypress",function(event){
 						return event.charCode>=48 && event.charCode<=57;
-					}).val((i==0?500:350));
+					}).val((i==0?100:100));
 				}
 				if(!$(".filldwn",this).length){
 					$(this).append(fldwn.clone()).find("input.filldwn").on("click",function(event){
 						event.stopPropagation();
-						thsamnt=$(this).parent().find(".buyamnt").val();
+						var thsamnt=$(this).parent().find(".buyamnt").val();
 						$("#products .product .buyamnt").each(function(){
 							$(this).val(thsamnt);
 						});
@@ -100,7 +74,7 @@ function _dodoo(obj){
 				if(!$(".checkall",this).length){
 					$(this).append(checkall.clone()).find("input.checkall").on("click",function(event){
 						event.stopPropagation();
-						thschck=$(this).prop("checked");
+						var thschck=$(this).prop("checked");
 						$("#products .product .buyque").each(function(){
 							$(this).prop("checked",thschck);
 						});
@@ -112,11 +86,11 @@ function _dodoo(obj){
 			if($("#dodooupgoo:checked").length>0 && $("#upgrades .upgrade.enabled").length>0)
 				$("#upgrades .upgrade.enabled:eq(0)").trigger({type:"click",which:1});
 			if($("#storeBulkBuy.selected").length>0 && $("#dodoobuy:checked").length>0 && $("#products .product.unlocked.enabled").length>0){
-				_=$("#products .product.unlocked.enabled").filter(function(){
+				var _=$("#products .product.unlocked.enabled").filter(function(){
 					return ($("input.buyque:checked",this).length === 1 && Number($("input.buyamnt",this).val())>0);
 				}).filter(($("#dodootop:checked").length?":first":":last"));
-				_source=Number($(".title.owned",_).text())||0;
-				_target=Number($("input.buyamnt",_).val());
+				var _source=Number($(".title.owned",_).text())||0;
+				var _target=Number($("input.buyamnt",_).val());
 				if(_target>_source){
 					$(_).trigger({type:"click",which:1});
 				}
@@ -135,4 +109,5 @@ function _dodoo(obj){
 		},500);
 	};
 	document.getElementsByTagName("head")[0].appendChild(script);
+})();
 })();
